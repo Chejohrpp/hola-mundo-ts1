fe626d076cb7d07a7cbd050a47db97d798047a6e
Chejohrpp <sergioroherp@gmail.com>
2021-09-06 13:41:49 -0600

    marco teorico

--- pages/helloworld.php
+++ pages/helloworld.php
@@ -19,6 +19,7 @@
     <audio id="song">
         <source src="../src/audios/one_last_kiss_01.mp3" type="audio/mp3">
     </audio>    
+    <!--Ejemplificacion de una de las caracteristicas mas usadas, del uso de una libreria -->
     <script src="../js/particles.js"></script>
     <script src="../js/particulas.js"></script>
     <script src="../js/script.js"></script>
